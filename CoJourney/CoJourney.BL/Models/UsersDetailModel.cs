0b39d1592ae54fd5733b118298b96e7df5063875
Jakub Dvořák <xdvora3q@stud.fit.vutbr.cz>
2022-04-10 23:28:45 +0200

    Zprovoznění hodně základních testů pro CarDetail

--- CoJourney/CoJourney.BL/Models/UsersDetailModel.cs
+++ CoJourney/CoJourney.BL/Models/UsersDetailModel.cs
@@ -20,7 +20,6 @@ namespace CoJourney.BL.Models
             {
                 CreateMap<UserEntity, UsersDetailModel>()
                     .ReverseMap()
-                    .ForMember(entity => entity.OwnedCars, expression => expression.Ignore())
                     .ForMember(entity => entity.DrivingJourneys, expression => expression.Ignore())
                     .ForMember(entity => entity.CoRidingJourneys, expression => expression.Ignore())
                     .ForMember(entity => entity.SentInvitations, expression => expression.Ignore())
